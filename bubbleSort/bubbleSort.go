3926dc7f4528234e82c2bd394c377e472f529765
KMimura <mimurakohei256@gmail.com>
2021-10-09 23:39:28 +0900

    add bubbleSort

---
+++ bubbleSort/bubbleSort.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	testVal := []int{5, 4, 7, 3, 0, 9, 2, 1, 8, 6}
+	resultVal := bubbleSort(testVal)
+	fmt.Println(resultVal)
+}
+
+func bubbleSort(slice []int) []int {
+	fmt.Println(len(slice))
+	for i := len(slice) - 1; i >= 1; i-- {
+		if slice[i-1] > slice[i] {
+			slice[i-1], slice[i] = slice[i], slice[i-1]
+		}
+	}
+	if len(slice) > 0 {
+		slice = append(slice[:1], bubbleSort(slice[1:])...)
+	}
+	return slice
+}
