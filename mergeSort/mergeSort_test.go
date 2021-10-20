47bdd69907b5644fed617d5b04d3d901691bb8a4
KMimura <mimurakohei256@gmail.com>
2021-10-20 11:12:15 +0900

    add a test case to mergeSort

--- mergeSort/mergeSort_test.go
+++ mergeSort/mergeSort_test.go
@@ -34,6 +34,23 @@ func TestMergeSort2(t *testing.T) {
 		}
 	}
 	if testFailureFlg {
-		t.Fatalf("test1 failed")
+		t.Fatalf("test2 failed")
+	}
+}
+
+func TestMergeSort3(t *testing.T) {
+	testCase := []int{0}
+	sortChan := make(chan []int)
+	go mergeSort(testCase, sortChan)
+	result := <-sortChan
+	testFailureFlg := false
+	expectedResult := []int{0}
+	for i, e := range expectedResult {
+		if result[i] != e {
+			testFailureFlg = true
+		}
+	}
+	if testFailureFlg {
+		t.Fatalf("test3 failed")
 	}
 }
